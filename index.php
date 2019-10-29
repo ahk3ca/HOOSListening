4873da1e36cb2a04470f52e3a278cf837d1af27d
ahk3ca <ahk3ca@virginia.edu>
2019-10-28 23:54:38 -0400

    merge stuff

--- index.php
+++ index.php
@@ -101,28 +101,82 @@ if(session_id() == '' || !isset($_SESSION)){session_start();}
 			    <p>Please fill in this form to create an account.</p>
 			    <hr>
 
-                <label for="email"><b>First Name</b></label>
-			    <input type="text" placeholder="Enter First Name" name="fname" required>
+                <label for="fname"><b>First Name</b></label>
+			    <input type="text" placeholder="Enter First Name" name="fname" required pattern = "^[a-zA-Z]+$" title = "Need one letter minimum. Nothing besides letters.">
 
-                <label for="email"><b>Last Name</b></label>
-			    <input type="text" placeholder="Enter Last Name" name="lname" required>
+                <label for="lname"><b>Last Name</b></label>
+			    <input type="text" placeholder="Enter Last Name" name="lname" required pattern = "^[a-zA-Z]+-*[a-zA-Z]*$" title = "Need one letter minimum. Nothing besides letters. Hyphens allowed.">
 
 			    <label for="email"><b>Email</b></label>
-			    <input type="text" placeholder="Enter Email" name="email" required>
+			    <input type="text" placeholder="Enter Email" name="email"required pattern = "^.+@+.+\.+.+$" value = "" title = "Please use valid email format. At least one . symbol and one @ symbol. >
 
-                <label for="email"><b>Address</b></label>
+                <label for="address"><b>Address</b></label>
 			    <input type="text" placeholder="Enter Address" name="address" required>
 
-                <label for="email"><b>City</b></label>
+                <label for="city"><b>City</b></label>
 			    <input type="text" placeholder="Enter City" name="city" required>
 
-                <label for="email"><b>State</b></label>
-			    <input type="text" placeholder="Enter State" name="state" required>
+                <label for="state"><b>State</b></label>
+                <select name = "state" required>
+							<option value = "">Make a Selection</option>
+							<option value="AL">Alabama</option>
+							<option value="AK">Alaska</option>
+							<option value="AZ">Arizona</option>
+							<option value="AR">Arkansas</option>
+							<option value="CA">California</option>
+							<option value="CO">Colorado</option>
+							<option value="CT">Connecticut</option>
+							<option value="DE">Delaware</option>
+							<option value="DC">District Of Columbia</option>
+							<option value="FL">Florida</option>
+							<option value="GA">Georgia</option>
+							<option value="HI">Hawaii</option>
+							<option value="ID">Idaho</option>
+							<option value="IL">Illinois</option>
+							<option value="IN">Indiana</option>
+							<option value="IA">Iowa</option>
+							<option value="KS">Kansas</option>
+							<option value="KY">Kentucky</option>
+							<option value="LA">Louisiana</option>
+							<option value="ME">Maine</option>
+							<option value="MD">Maryland</option>
+							<option value="MA">Massachusetts</option>
+							<option value="MI">Michigan</option>
+							<option value="MN">Minnesota</option>
+							<option value="MS">Mississippi</option>
+							<option value="MO">Missouri</option>
+							<option value="MT">Montana</option>
+							<option value="NE">Nebraska</option>
+							<option value="NV">Nevada</option>
+							<option value="NH">New Hampshire</option>
+							<option value="NJ">New Jersey</option>
+							<option value="NM">New Mexico</option>
+							<option value="NY">New York</option>
+							<option value="NC">North Carolina</option>
+							<option value="ND">North Dakota</option>
+							<option value="OH">Ohio</option>
+							<option value="OK">Oklahoma</option>
+							<option value="OR">Oregon</option>
+							<option value="PA">Pennsylvania</option>
+							<option value="RI">Rhode Island</option>
+							<option value="SC">South Carolina</option>
+							<option value="SD">South Dakota</option>
+							<option value="TN">Tennessee</option>
+							<option value="TX">Texas</option>
+							<option value="UT">Utah</option>
+							<option value="VT">Vermont</option>
+							<option value="VA">Virginia</option>
+							<option value="WA">Washington</option>
+							<option value="WV">West Virginia</option>
+							<option value="WI">Wisconsin</option>
+							<option value="WY">Wyoming</option>
+			    </select>
+			    			
 
-                <label for="email"><b>5-digit zip code</b></label>
+                <label for="zipcode"><b>5-digit zip code</b></label>
 			    <input type="text" placeholder="Enter 5-digit zip code" name="zipcode" required>
 
-                <label for="email"><b>Username</b></label>
+                <label for="username"><b>Username</b></label>
 			    <input type="text" placeholder="Enter Username" name="username" required>
 
 			    <label for="psw"><b>Password</b></label>
--- signup.php
+++ signup.php
@@ -1,5 +1,15 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+require 'vendor/PHPMailer/phpmailer/src/PHPMailer.php';
+require 'vendor/PHPMailer/phpmailer/src/SMTP.php';
+
 include 'config.php';
+include 'index.php';
 $fname = $_POST["fname"];
 $lname = $_POST["lname"];
 $email = $_POST["email"];
@@ -10,13 +20,35 @@ $zipcode = $_POST["zipcode"];
 $username = $_POST["username"];
 $psw = $_POST["psw"];
 
-// $query -> execute();
 
 // $getusers = pg_query($db_connection, "SELECT * FROM \"Users\"");
 
 $query = "INSERT INTO \"Users\" VALUES ('$fname', '$lname', '$email', '$address', '$city', '$state', '$zipcode', '$username', '$psw')";
 
 $result = pg_query($db_connection, $query);
+
+
+
+		try{
+		$mail = new PHPMailer(TRUE);
+	  	$mail->isSMTP();          
+	  	$mail->Host 	  = 'smtp.gmail.com';         
+		$mail->SMTPAuth   = true;         
+		$mail->SMTPSecure = 'tls';         
+		$mail->Port       = 587;             
+		$mail->Username   = "ecomm.hooslistening@gmail.com";          
+		$mail->Password   = "temppassword!";                 
+        $mail->setFrom('ecomm.hooslistening@gmail.com', 'HOOSListening');
+        $mail->addAddress("$email");
+        $mail->Subject = 'Welcome to HOOSListening!';
+        $mail->Body = 'Thanks for signing up with HOOSListening!';
+        $mail->send();
+
+		exit();
+	}
+	catch (Exception $e) {
+
+	};
 
 
 // if($db_connection->pg_query("INSERT INTO Users ('fname', 'lname', 'email', 'address', 'city', 'state', 'zipcode', 'username', 'psw') VALUES($fname, $lname, $email ,$address, $city, $state, $zipcode, $username, $psw)")){
